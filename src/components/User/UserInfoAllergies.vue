c342563044295282db9b288e357e88c5d5d0b97b
John <jaboyak@gmail.com>
2017-08-13 15:30:35 -0700

    updated allergies page

--- src/components/User/UserInfoAllergies.vue
+++ src/components/User/UserInfoAllergies.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="user-info-allergies">
     <h1>{{ msg }}</h1>
+    <form>
+      <p>
+       <input type="checkbox" id="test5" />
+       <label for="test5">Patient does not have any allergies</label>
+      </p>
+    </form>
   </div>
 </template>
 
