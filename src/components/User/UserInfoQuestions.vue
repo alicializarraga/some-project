72072813d9bf611cac46b68205d5cfff9f955a68
Alicia Lizarraga <alizarraga@mhchealthcare.org>
2017-08-13 20:09:58 -0700

    Adding Question Form

--- src/components/User/UserInfoQuestions.vue
+++ src/components/User/UserInfoQuestions.vue
@@ -1,6 +1,45 @@
 <template>
   <div class="user-info-questions">
     <h1>{{ msg }}</h1>
+
+    <p><label class="plainText"><strong>Have you ever abused any substances or drugs</strong></label></p>
+
+    <input name="qx1" type="radio" id="r1">
+    <label for="r1">Yes</label>
+
+    <input name="qx1" type="radio" id="r2">
+    <label for="r2">No</label>
+    <br>
+    <p><label class="plainText"><strong>Do you have a history of psychiatric disorders or hospitalizations</strong></label></p>
+
+    <input name="qx2" type="radio" id="r1">
+    <label for="r1">Yes</label>
+
+    <input name="qx2" type="radio" id="r2">
+    <label for="r2">No</label>
+    <br>
+    <p><label class="plainText"><strong>Do you have a history of suicidal ideations or attempts</strong></label></p>
+
+    <input name="qx3" type="radio" id="r1">
+    <label for="r1">Yes</label>
+
+    <input name="qx3" type="radio" id="r2">
+    <label for="r2">No</label>
+    <br>
+    <p><label class="plainText"><strong>Does your family have a history of suicidal attempts</strong></label></p>
+
+    <input name="qx4" type="radio" id="r1">
+    <label for="r1">Yes</label>
+
+    <input name="qx4" type="radio" id="r2">
+    <label for="r2">No</label>
+    <p> <div class="row">
+          <div class="col s12">
+              <router-link class="btn" to="pharmacy">Continue</router-link>
+          </div>
+        </div>
+    </p>
+
   </div>
 </template>
 
@@ -17,5 +56,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.plainText {
+  color: #46B4BB;
+  font-size: 18px;
+}
 </style>
